f0ad60c25d953bc56059fd6126144b3f6c8f3f30
Denis O <denis@universal-development.com>
2017-02-12 09:29:15 +0200

    Added poly stats service

---
+++ src/main/java/com/unidev/PolyStatsApplication.java
@@ -0,0 +1,12 @@
+package com.unidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyStatsApplication.class, args);
+	}
+}
